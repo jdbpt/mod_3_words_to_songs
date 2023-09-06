9bf48d22f3fc685197ab4824422486e11db86040
jdbpt <jbrown5.filn@gmail.com>
2023-09-06 16:34:37 -0400

    update logo in signup/signin

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -2,13 +2,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import SignHeader from '../components/SignHeader';
 import "./pages.css";
+import logo from "../images/wordToSongsLogo.png"
+
 
 const SignIn = () => {
   return (
     <div>
       <SignHeader/>
       <div className='sign'>
-        <h3>Wecome back!</h3>
+        <h2>Wecome back!</h2>
+        <img className="bigLogo" src={logo} alt="logo Words to Songs" />
+
         <Link to="/home">Home</Link>
         <h3>New User?</h3>
         <Link to="/signup">Sign Up</Link>
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -2,6 +2,7 @@ import React from 'react';
 import SignHeader from '../components/SignHeader';
 import { Link } from 'react-router-dom';
 import "./pages.css";
+import logo from "../images/wordToSongsLogo.png"
 
 const SignUp = () => {
   return (
@@ -10,6 +11,8 @@ const SignUp = () => {
       <div className='sign'>
         <h2>Sign Up</h2>
         <h2>Welcome!</h2>
+        <img className="bigLogo" src={logo} alt="logo Words to Songs" />
+
         <Link to="/home">Home</Link>
       </div>
       
